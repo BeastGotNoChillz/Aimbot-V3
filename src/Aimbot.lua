8b508f37951908134a5f42eac103dd18cea3c11f
Aditya Bam <167054902+BeastGotNoChillz@users.noreply.github.com>
2025-06-02 11:50:16 +0545

    Update Aimbot mouse buttton 5 bind.lua

--- src/Aimbot.lua
+++ src/Aimbot.lua
@@ -109,7 +109,7 @@ getgenv().ExunysDeveloperAimbot = {
 		LockMode = 1, -- 1 = CFrame; 2 = mousemoverel
 		LockPart = "Head", -- Body part to lock on
 
-		TriggerKey = Enum.UserInputType.MouseButton2,
+		TriggerKey = Enum.UserInputType.MouseButton5,
 		Toggle = false
 	},
 
@@ -117,7 +117,7 @@ getgenv().ExunysDeveloperAimbot = {
 		Enabled = true,
 		Visible = true,
 
-		Radius = 90,
+		Radius = 100,
 		NumSides = 60,
 
 		Thickness = 1,
